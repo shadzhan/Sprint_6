68bb75ba98d810e54a0c95aa23263bfb25e6ba5b
Albert Shadzhanov <albertm79@yandex.ru>
2025-05-15 04:33:06 +0300

    Written tests to fill forms

--- tests/test_order.py
+++ tests/test_order.py
@@ -3,53 +3,63 @@ import allure
 from pages.main_page import MainPage
 from pages.order_page import OrderPage
 from data import Data
-from curl import main_site
 from locators.main_page_locators import MainPageLocators
-from helper import generate_registration_data
+from locators.order_page_locators import OrderPageLocators
 
 
 
 @allure.feature("Заказ самоката")
 class TestOrder:
-
-    @pytest.mark.parametrize("button_locator", [
-        MainPageLocators.ORDER_BUTTON_TOP,
-        MainPageLocators.ORDER_BUTTON_BOTTOM
-    ], ids=["top_button", "bottom_button"])
-    def test_order_flow(self, driver, button_locator):
-        with allure.step("Инициализация страниц и данных"):
+    @pytest.mark.parametrize("name,surname,address,station,phone,date,period", [
+        (   "Михаил",
+            "Волков",
+            "ул. Лермонтова, 27",
+            "Фрунзенская",
+            "89171234598",
+            "21.05.2025",
+            "трое суток"
+        ),
+        (
+            "Петр",
+            "Фёдоров",
+            "ул. Пушкина, 49",
+            "Кремлевская",
+            "89054223665",
+            "20.05.2025",
+            "сутки"
+        )
+    ])
+    def test_order_flow(self, driver, name, surname, address, station, phone, date, period):
+        with allure.step("1. Открытие главной страницы"):
             main_page = MainPage(driver)
+            main_page.open(Data.main_site)
+
+        with allure.step("2. Принятие куки"):
+            main_page.accept_cookies()
+
+        with allure.step("3. Нажатие на кнопку 'Заказать'"):
+            main_page.click_order_button()
+
+        with allure.step("4. Заполнение формы 'Для кого самокат'"):
             order_page = OrderPage(driver)
-            user_data = Data.get_test_user()
+            order_page.fill_name(name)
+            order_page.fill_surname(surname)
+            order_page.fill_address(address)
+            order_page.fill_metro_station(station)
+            order_page.fill_phone(phone)
 
-        with allure.step("1. Принимаем куки"):
-            main_page.click_on_cookia_button()
-
-        button_info = {
-            MainPageLocators.ORDER_BUTTON_TOP: "Верхняя кнопка 'Заказать'",
-            MainPageLocators.ORDER_BUTTON_BOTTOM: "Нижняя кнопка 'Заказать'"
-        }
-
-        with allure.step(f"2. Нажимаем {button_info[button_locator]}"):
-            main_page.click_order_button(button_locator)
-
-        with allure.step("3. Заполняем форму 'Для кого самокат'"):
-            order_page.fill_recipient_form(user_data)
-
-        with allure.step("4. Нажимаем кнопку 'Далее'"):
+        with allure.step("5. Нажатие кнопки 'Далее'"):
             order_page.click_next_button()
 
-        with allure.step("5. Заполняем форму 'Про аренду'"):
-            order_page.fill_rental_details(user_data)
+        with allure.step("6. Заполнение формы 'Про аренду'"):
+            order_page.fill_delivery_date(date)
+            order_page.select_rental_period(period)
 
-        with allure.step("6. Нажимаем кнопку 'Заказать'"):
+        with allure.step("7. Нажатие кнопки 'Заказать'"):
             order_page.click_order_button()
 
-        with allure.step("7. Подтверждаем заказ"):
-            order_page.click_confirm_button()
+        with allure.step("8. Подтверждение заказа"):
+            order_page.confirm_order()
 
-        with allure.step("8. Проверяем подтверждение заказа"):
-            assert order_page.verify_order_confirmation()
-
-        with allure.step("9. Нажимаем кнопку 'Посмотреть статус'"):
-            order_page.click_view_status_button()
+        with allure.step("9. Проверка подтверждения заказа"):
+            assert "Заказ оформлен" in order_page.get_success_message()
