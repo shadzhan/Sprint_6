4bd8f53acba517230f1702e17c6e8ce7e093b8fd
Albert Shadzhanov <albertm79@yandex.ru>
2025-05-15 04:31:03 +0300

    Created tests for checking questions and answers

--- tests/test_faq.py
+++ tests/test_faq.py
@@ -12,7 +12,7 @@ class TestFaqSection:
     @pytest.mark.parametrize("number, expected_text", Data.accordion_names)
     def test_faq_answers(self, driver, number, expected_text):
         main_page = MainPage(driver)
-        main_page.scroll_to_element(MainPageLocators.faq_questions_items[number])
+        main_page.scroll_to_element(MainPageLocators.faq_questions_items(number))
         main_page.click_on_faq_question(number)
         main_page.wait_for_faq_answer(number)
         actual_text = main_page.get_faq_answer_text(number)
