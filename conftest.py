d452cc5b7d01925a5f51585a1b7b6b440cd044d7
Albert Shadzhanov <albertm79@yandex.ru>
2025-05-16 23:25:22 +0300

    Deleted unnecessary data from the file

--- conftest.py
+++ conftest.py
@@ -12,12 +12,3 @@ def driver():
     yield driver
     driver.quit()
 
-@pytest.fixture
-def main_page(driver):
-    from pages.main_page import MainPage
-    return MainPage(driver)
-
-@pytest.fixture
-def order_page(driver):
-    from pages.order_page import OrderPage
-    return OrderPage(driver)
